60ad7eaa101933da35d650553edbb0f3deab83fd
MuazzezA <ayd.muazzez@gmail.com>
2023-12-23 18:03:41 +0300

    feat: create project

---
+++ StepCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StepCounter
+//
+//  Created by Muazzez Aydın on 23.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
